0858311786151ad2ee56bd8a3f57ece0c14ff359
wzx <1257593080@qq.com>
2020-08-25 16:23:19 +0800

    修改ColaConstant常量

--- cola-framework/cola-core/src/main/java/com/baosiling/cola/common/ColaConstant.java
+++ cola-framework/cola-core/src/main/java/com/baosiling/cola/common/ColaConstant.java
@@ -6,8 +6,8 @@ package com.baosiling.cola.common;
  * @create: 2020-08-22 15:04
  */
 public class ColaConstant {
-    public final static String DOT_SEPARATOR = ",";
-    public final static String EXTENSION_EXTPT_NAMING = "ExPt";
+    public final static String DOT_SEPARATOR = ".";
+    public final static String EXTENSION_EXTPT_NAMING = "ExtPt";
 
     public final static String EXE_METHOD = "execute";
 }
