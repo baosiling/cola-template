d4847176354085b2a46733771da8b5b55df6b4e3
nalo <1257593080@qq.com>
2020-09-06 22:35:34 +0800

    Amazon

--- eureka-framework/eureka-client/src/main/java/com/netflix/appinfo/AmazonInfo.java
+++ eureka-framework/eureka-client/src/main/java/com/netflix/appinfo/AmazonInfo.java
@@ -1,5 +1,9 @@
 package com.netflix.appinfo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
 /**
  * @description:
  * @author: wangzhx
@@ -20,7 +24,13 @@ public class AmazonInfo implements DataCenterInfo, UniqueIdentifier {
             this.name = name;
             this.path = path;
         }
+
+        public String getName(){
+            return name;
+        }
     }
+
+    private Map<String, String> metadata;
 
     @Override
     public Name getName() {
@@ -34,5 +44,10 @@ public class AmazonInfo implements DataCenterInfo, UniqueIdentifier {
 
     public String get(MetaDataKey key){
         return null;
+    }
+
+    @JsonProperty("metadata")
+    public Map<String, String> getMetadata(){
+        return metadata;
     }
 }
